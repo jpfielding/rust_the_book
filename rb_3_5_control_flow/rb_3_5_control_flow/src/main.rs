b98acfc4fc5b08e441768b9214e86988edd96cdb
JP Fielding <fieldingj@leidos.com>
2025-08-30 23:26:59 +0000

    rust the book

--- rb_3_5_control_flow/rb_3_5_control_flow/src/main.rs
+++ rb_3_5_control_flow/rb_3_5_control_flow/src/main.rs
@@ -18,4 +18,89 @@ fn main() {
     } else {
         println!("number is not divisible by 4, 3, or 2");
     }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+
+    lool_return_val();
+    labeled_loops();
+    conditional_loop();
+    for_loop();
+    for_collection();
+    for_reverse();
+}
+
+fn lool_return_val() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+
+fn labeled_loops() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+fn conditional_loop() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+fn for_loop() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
+fn for_collection() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+}
+
+fn for_reverse() {
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
