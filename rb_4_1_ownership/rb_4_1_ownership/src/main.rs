8ea9adfdb3bf8028638f4a71087d1646e4afd545
JP Fielding <jp.fielding@strongdm.com>
2025-08-31 22:22:29 +0000

    ownership

--- rb_4_1_ownership/rb_4_1_ownership/src/main.rs
+++ rb_4_1_ownership/rb_4_1_ownership/src/main.rs
@@ -9,6 +9,8 @@ fn main() {
     clone_example();
     stack_only_data();
     ownership_and_functions();
+    return_values_scope();
+    move_len_calc();
 }
 
 
@@ -67,3 +69,32 @@ fn makes_copy(some_integer: i32) {
     println!("{}", some_integer);
 } // some_integer goes out of scope here, but nothing special happens
 
+fn return_values_scope() {
+    let _s1 = gives_ownership(); // gives_ownership moves its return value into s1
+    let s2 = String::from("hello"); // s2 comes into scope
+    let _s3 = takes_and_gives_back(s2); // s2 is moved into takes_and_gives_back, which also moves its return value into s3
+} // s3 goes out of scope and is dropped. s2 was moved, so nothing happens. s1 goes out of scope and is dropped.
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello"); // some_string comes into scope
+    some_string // some_string is returned and moves out to the calling function
+}
+
+fn takes_and_gives_back(some_string: String) -> String {
+    println!("{}", some_string);
+    String::from("goodbye")
+}
+
+fn move_len_calc() {
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{s2}' is {len}.");
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+
+    (s, length)
+}
