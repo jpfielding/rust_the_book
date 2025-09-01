1064ed9247f72f5cc8018cdb39d326943097d6cd
JP Fielding <fieldingj@leidos.com>
2025-09-01 01:34:00 +0000

    cas

--- rb_4_2_refs_borrow/rb_4_2_refs_borrow/src/main.rs
+++ rb_4_2_refs_borrow/rb_4_2_refs_borrow/src/main.rs
@@ -1,23 +1,25 @@
 fn main() {
-    let s = String::from("hello");  // s comes into scope
+    refs_borrow();
+    mut_ref();
+}
 
-    takes_ownership(s);             // s's value moves into the function...
-                                    // ... and so is no longer valid here
+fn refs_borrow() {
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of '{s1}' is {len}.");
+}
 
-    let x = 5;                      // x comes into scope
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
 
-    makes_copy(x);                  // Because i32 implements the Copy trait,
-                                    // x does NOT move into the function,
-                                    // so it's okay to use x afterward.
+fn mut_ref() {
+    let mut s = String::from("hello"); // mutable reference
 
-} // Here, x goes out of scope, then s. However, because s's value was moved,
-  // nothing special happens.
+    change(&mut s); // mutable reference
+}
 
-fn takes_ownership(some_string: String) { // some_string comes into scope
-    println!("{some_string}");
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
+fn change(some_string: &mut String) {
+    some_string.push_str(", world"); // mutable reference
+}
 
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
-    println!("{some_integer}");
-} // Here, some_integer goes out of scope. Nothing special happens.
